53d145516344ebf097eaf0ae769473ee32d39665
Felice Forby <feliceforby@gmail.com>
2018-07-02 23:27:57 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,15 @@
 class Dog
   attr_accessor :name, :age
 
+  @@all = []
+  
   def initialize(:name, :age)
     @name = name
     @age = age
+    @@all << self
   end
-  
+
+  def self.all
+    @@all
+  end
 end
